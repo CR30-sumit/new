9a7adca28fd7aec58742d70180cc3a42f311e712
sumit <s.sumit@globaledgesoft.com>
2021-02-17 10:50:22 +0530

    added

---
+++ training/experiment/basic/hel.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//int x;
+//int j;
+int main()
+{
+		//static int y;
+		//static int x;
+		printf("hello");
+		//int x;
+		//printf("Enter the input\n");
+		//scanf("%d",&x);
+		return 0;
+}
